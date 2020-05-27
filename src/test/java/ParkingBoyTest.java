38f4b480d1644236dce187aa6897603fdd54b491
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-05-27 10:38:55 +0800

    finished step 2

--- src/test/java/ParkingBoyTest.java
+++ src/test/java/ParkingBoyTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ParkingBoyTest {
     @Test
     public void ShouldParkTheCarInTheFirstParkingLotIfAvailable() throws Exception {
-        List<ParkingLot> parkingLotList = new ArrayList<ParkingLot>();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
         parkingLotList.add(new ParkingLot(2));
         parkingLotList.add(new ParkingLot(2));
         ParkingBoy boy = new ParkingBoy(parkingLotList);
@@ -16,4 +16,16 @@ public class ParkingBoyTest {
         Assert.assertEquals(boy.getParkingLotList().get(1).getSpaces(), 2);
     }
 
+    @Test
+    public void ShouldParkTheCarInTheSecondParkingLotIfTheFirstParkingLotIsUnavailable() throws Exception {
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(new ParkingLot(2));
+        parkingLotList.add(new ParkingLot(2));
+        ParkingBoy boy = new ParkingBoy(parkingLotList);
+        boy.park();
+        boy.park();
+        boy.park();
+        Assert.assertEquals(boy.getParkingLotList().get(0).getSpaces(), 0);
+        Assert.assertEquals(boy.getParkingLotList().get(1).getSpaces(), 1);
+    }
 }
