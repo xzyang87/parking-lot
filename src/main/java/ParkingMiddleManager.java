dc29123abaa50b5d58049d7e072ccf321a45e2e2
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-05-27 16:50:16 +0800

    Finished Step 3

---
+++ src/main/java/ParkingMiddleManager.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class ParkingMiddleManager {
+    private List<ParkingLot> parkingLots;
+
+    public ParkingMiddleManager(List<ParkingLot> parkingLots) {
+
+        this.parkingLots = parkingLots;
+    }
+
+    public void park() throws Exception {
+        if (parkingLots.size() > 0) {
+             ParkingLot lotToPark = parkingLots.stream().max(Comparator.comparingInt(ParkingLot::getSpaces)).get();
+             lotToPark.park();
+        }
+    }
+}
---
+++ src/test/java/ParkingMiddleManagerTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ParkingMiddleManagerTest {
+    @Test
+    public void ShouldParkTheCarInTheFirstParkingLotIfTheFirstParkingLotHasMoreAvailableSpaces() throws Exception {
+        ParkingLot lot1 = new ParkingLot(5);
+        ParkingLot lot2 = new ParkingLot(3);
+        List<ParkingLot> parkingLots = Arrays.asList(lot1, lot2);
+
+        ParkingMiddleManager middleManager = new ParkingMiddleManager(parkingLots);
+        middleManager.park();
+        Assert.assertEquals(lot1.getSpaces(), 4);
+        Assert.assertEquals(lot2.getSpaces(), 3);
+    }
+}
